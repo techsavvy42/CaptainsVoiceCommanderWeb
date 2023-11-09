87d9bb633c72f79f7cdc814eddcf891f4d12129c
techsavvy42 <120347226+techsavvy42@users.noreply.github.com>
2023-11-09 12:17:06 -0700

    Update Disclaimer.tsx

--- src/pages/Disclaimer/Disclaimer.tsx
+++ src/pages/Disclaimer/Disclaimer.tsx
@@ -27,10 +27,10 @@ The words of which the initial letter is capitalized have meanings defined under
 
 For the purposes of this Disclaimer:
 
-*   **Project** (referred to as either "the Project", "We", "Us" or "Our" in this Disclaimer) refers to Chitchatter.
+*   **Project** (referred to as either "the Project", "We", "Us" or "Our" in this Disclaimer) refers to chat-test-nine-psi.
 *   **Service** refers to the Website.
 *   **You** means the individual accessing the Service, or the company, or other legal entity on behalf of which such individual is accessing or using the Service, as applicable.
-*   **Website** refers to Chitchatter, accessible from [https://chitchatter.im](https://chitchatter.im)
+*   **Website** refers to chat-test-nine-psi, accessible from [https://chat-test-nine-psi.vercel.app](https://chat-test-nine-psi.vercel.app)
 
 ### Disclaimer
 
